f8e45ced2f15838614d44d1accdf2b3e121ab0ca
Riddhi73 <riddhimandebnath4260@gmail.com>
2026-09-02 12:08:59 +0600

    Add customization

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,7 +8,7 @@ export default function Home() {
   return (
     <div className="flex flex-col gap-y-4">
       <div>
-        <Button>I am a button</Button>
+        <Button variant="elevated">I am a button</Button>
       </div>
       <div>
         <Input placeholder="I am an input"></Input>
