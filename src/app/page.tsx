f57a3e597090251a82b101cf714f1c54055ba83a
Riddhi73 <riddhimandebnath4260@gmail.com>
2026-09-02 10:34:04 +0600

    Change border & input color

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,27 @@
 import { Button } from "@/components/ui/button";
+import { Checkbox } from "@/components/ui/checkbox";
+import { Input } from "@/components/ui/input";
+import { Progress } from "@/components/ui/progress";
+import { Textarea } from "@/components/ui/textarea";
 
 export default function Home() {
   return (
-    <div>
-      <Button variant={"destructive"}>Hello world</Button>
-      <p className="text-rose-500">Hello world</p>
+    <div className="flex flex-col gap-y-4">
+      <div>
+        <Button>I am a button</Button>
+      </div>
+      <div>
+        <Input placeholder="I am an input"></Input>
+      </div>
+      <div>
+        <Progress value={50} />
+      </div>
+      <div>
+        <Textarea placeholder="I am a textarea" />
+      </div>
+      <div>
+        <Checkbox />
+      </div>
     </div>
   );
 }
